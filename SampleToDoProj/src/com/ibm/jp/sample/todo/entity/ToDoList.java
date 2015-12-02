c9f604159dcc1ddb685b94b831f18d5ec085a212
capsmalt <capsmalt@gmail.com>
2015-12-02 11:06:39 +0900

    This is a original version of SampleToDoProj.

---
+++ SampleToDoProj/src/com/ibm/jp/sample/todo/entity/ToDoList.java
@@ -0,0 +1,25 @@
+package com.ibm.jp.sample.todo.entity;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+
+@ApplicationScoped
+@Named("todolist")
+public class ToDoList extends ArrayList<ToDoEntry> {	
+	
+	public ToDoList() {
+	}
+	
+	public void removeSchedule(String id) {
+		Iterator<ToDoEntry>it = this.iterator();
+		while (it.hasNext()) {		
+			ToDoEntry entry = it.next();
+			if (entry.getId().equals(id)) {
+				it.remove();
+			}
+		}
+	}
+	
+}
